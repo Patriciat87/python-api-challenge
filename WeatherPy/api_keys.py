3808f324e7507096674e2c786cabf7d4d7abfc9b
Patricia Taylor <Patriciat87@yahoo.com>
2024-08-09 19:08:58 -0500

    update

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -2,9 +2,8 @@
 
 
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "6672fb12e271f01172cdf7c314b0aab8"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "6f7b745acd0843b298ccd1e4abeb3233"
 
-TESTTTTT
